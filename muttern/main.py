e81cc451c2c4f7e4fd06f9c7e6336ec500d23fcd
henriman <henri.inndorf@tutanota.com>
2021-03-14 00:38:16 +0100

    Add `screen_size`, `resolution` and `framerate` to `config.ini`

--- muttern/main.py
+++ muttern/main.py
@@ -6,6 +6,18 @@ import imutils.video as video
 import cv2
 import database
 from pyzbar import pyzbar
+import configparser
+from typing import Callable, Any
+
+def str_to_tuple(s: str, f: Callable[[str], Any]):
+    return tuple(map(f, s.strip("()").split(", ")))
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+screen_size = str_to_tuple(config["hardware"]["screen_size"], int)
+resolution = str_to_tuple(config["hardware"]["resolution"], int)
+framerate = config["hardware"].getint("framerate")
 
 def stream():
     # Grab the current frame of the video stream.
@@ -23,6 +35,13 @@ def stream():
     # Find and decode the barcodes in the frame.
     barcodes = pyzbar.decode(frame)
 
+    if not barcodes:
+        # Continue streaming video.
+        image.after(delay, stream)
+        confirm_button.config(state=tk.DISABLED)
+    else:
+        confirm_button.config(state=tk.NORMAL)
+
     for barcode in barcodes:
         # Convert the barcode data into a string.
         barcode_data = barcode.data.decode("utf-8")
@@ -34,13 +53,12 @@ def stream():
             brand_text.set(product.brands)
             name_text.set(product.name)
 
-    # Continue streaming video.
-    image.after(delay, stream)
-
 # Configure window.
 root = tk.Tk()
 root.title('muttern')
-#root.attributes("-fullscreen", True)
+root.geometry(f"{screen_size[0]}x{screen_size[1]}")
+# TODO: translate to fullscreen
+# root.attributes("-fullscreen", True)
 root.bind("<Escape>", lambda _: root.destroy())
 
 # Variables.
@@ -50,17 +68,29 @@ name_text = tk.StringVar()
 
 # Add the widgets.
 image = tk.Label()
-image.pack()
+image.grid(rowspan=5)
 
 barcode_label = tk.Label(root, textvariable=barcode_text)
-barcode_label.pack()
+barcode_label.grid(row=0, column=1, sticky="N")
 brand_label = tk.Label(root, textvariable=brand_text)
-brand_label.pack()
+brand_label.grid(row=1, column=1, sticky="N")
 name_label = tk.Label(root, textvariable=name_text)
-name_label.pack()
+name_label.grid(row=2, column=1, sticky="N")
+
+confirm_button = tk.Button(
+    root,
+    state=tk.DISABLED,
+    text="Confirm",
+    command=lambda: image.after(delay, stream)
+)
+confirm_button.grid(row=3, column=1, sticky="N")
 
 # Initialize the video stream.
-video_stream = video.VideoStream(usePiCamera=True, resolution=(640, 480)).start()
+video_stream = video.VideoStream(
+    usePiCamera=True,
+    framerate=framerate,
+    resolution=resolution
+).start()
 # TODO: let sleep asynchronously?
 time.sleep(2)  # Allow the camera sensor to warm up.
 
