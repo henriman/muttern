8afd9a794185fd33ac5e8b433047db61de92e976
henriman <henri.inndorf@tutanota.com>
2021-04-04 14:30:57 +0200

    Add type hint to `str_to_tuple`

--- muttern/config.py
+++ muttern/config.py
@@ -1,11 +1,12 @@
 """This module includes helper functions for the config parser."""
 
-from typing import Callable, Any
+from typing import Callable, Any, TypeVar, Tuple
 
-def str_to_tuple(s: str, t: Callable[[str], Any]):
+T = TypeVar("T")
+
+def str_to_tuple(s: str, t: Callable[[str], T]) -> Tuple[T, ...]:
     """Parse a string into a tuple of type `t`."""
 
     return tuple(map(t, s.strip("()").split(", ")))
 
-# TODO: type hint here
 # TODO: product.py: self.lc as class variable?
