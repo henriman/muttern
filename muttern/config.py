49481945e3d76c44f3c2851ec00bc8e8a9607605
henriman <henri.inndorf@tutanota.com>
2021-05-16 12:04:32 +0200

    Remove comment

--- muttern/config.py
+++ muttern/config.py
@@ -8,5 +8,3 @@ def str_to_tuple(s: str, t: Callable[[str], T]) -> Tuple[T, ...]:
     """Parse a string into a tuple of type `t`."""
 
     return tuple(map(t, s.strip("()").split(", ")))
-
-# TODO: product.py: self.lc as class variable?
