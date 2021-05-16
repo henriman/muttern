b77c391f632942758e27053220302b8fa57dc77c
henriman <henri.inndorf@tutanota.com>
2021-05-16 23:10:59 +0200

    Put context managers for barcode scanner in `MainGUI`

--- muttern/gui/__init__.py
+++ muttern/gui/__init__.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import gui.labels
 import configparser
+import database
 from typing import Any
 import barcode_scanner
 import config as cfg
@@ -11,15 +12,6 @@ import gui.frames
 
 class MainGUI(tk.Tk):
     """The barcode scanner GUI."""
-
-    configuration = configparser.ConfigParser()
-    configuration.read("config.ini")
-
-    screen_size = cfg.str_to_tuple(configuration["hardware"]["screen_size"], int)
-    resolution = cfg.str_to_tuple(configuration["hardware"]["resolution"], int)
-    framerate = configuration["hardware"].getint("framerate")
-
-    stream_delay = int(1000 / framerate)
 
     def __init__(self, barcode_scanner: barcode_scanner.BarcodeScanner) -> None:
         """Initialize the GUI."""
@@ -36,6 +28,8 @@ class MainGUI(tk.Tk):
         # root.attributes("-fullscreen", True)
         self.bind("<Escape>", lambda _: self.destroy())
 
-        self.bs_frame = frames.BarcodeScannerFrame(self, barcode_scanner)
-        self.bs_frame.pack()
-        self.bs_frame.stream()
+        with database.OFFDatabaseHandler(cache_location=None) as dbh:
+            with barcode_scanner.BarcodeScanner(dbh) as scanner:
+                bs_frame = frames.BarcodeScannerFrame(self, barcode_scanner)
+                bs_frame.pack()
+                bs_frame.stream()
--- muttern/main.py
+++ muttern/main.py
@@ -1,11 +1,7 @@
 """Starting point for the application."""
 
-import database
-import barcode_scanner
 import gui
 
 if __name__ == "__main__":
-    with database.OFFDatabaseHandler(cache_location=None) as dbh:
-        with barcode_scanner.BarcodeScanner(dbh) as scanner:
             root = gui.MainGUI(scanner)
             root.mainloop()
