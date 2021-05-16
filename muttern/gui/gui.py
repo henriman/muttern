be54475786ee1cdac05521ceb524cb628e7dce09
henriman <henri.inndorf@tutanota.com>
2021-05-16 12:51:43 +0200

    Re-organize GUI code into sub directory with `gui.py` and `labels.py`

--- muttern/gui/gui.py
+++ muttern/gui/gui.py
@@ -8,17 +8,6 @@ import barcode_scanner
 import config as cfg
 import numpy
 
-class ImageLabel(tk.Label):
-    """A label with an updatable image."""
-
-    def update_image(self, image: numpy.ndarray) -> None:
-        """Update the label with the given image."""
-
-        display_image = Image.fromarray(image)
-        display_image = ImageTk.PhotoImage(display_image)
-        self.config(image=display_image)
-        self.image = display_image
-
 class BarcodeScannerGUI(tk.Tk):
     """The barcode scanner GUI."""
 
---
+++ muttern/gui/labels.py
@@ -0,0 +1,10 @@
+class ImageLabel(tk.Label):
+    """A label with an updatable image."""
+
+    def update_image(self, image: numpy.ndarray) -> None:
+        """Update the label with the given image."""
+
+        display_image = Image.fromarray(image)
+        display_image = ImageTk.PhotoImage(display_image)
+        self.config(image=display_image)
+        self.image = display_image
